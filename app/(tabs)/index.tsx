3c53a476d9729d63e6c4a77cd276c860b707c977
Jason Carzano <json.carzano@gmail.com>
2026-08-10 14:49:44 +0800

    [Feature][Jason] Add home page feature

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,31 @@
+import { HOME_BALANCE } from '@/constants/data';
+import { icons } from '@/constants/icons';
+import images from '@/constants/images';
 import '@/global.css'
-import { Link } from "expo-router";
+import { formatCurrency } from '@/lib/utils';
 import { styled } from "nativewind"
-import { Text, View } from "react-native";
+import {Image, View, Text } from "react-native";
 import { SafeAreaView as RNSafeReaView} from 'react-native-safe-area-context';
  
 const SafeAreaView = styled(RNSafeReaView)
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center p-5 justify-center bg-[#fff7e5]">
-      <Text className="text-xl font-bold text-[#16a34a]">
-        Welcome to Shushu and Sasha!
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
+    <SafeAreaView className="flex-1 p-5">
+     <View className='home-header'>
+      <View className='home-user'>
+        <Image source={images.unnamed} className="home-avatar"/>
+        <Text className='home-user-name'>Jason Carzano</Text>
+      </View>
+
+      <Image source={icons.add} className='home-add-icon'/>
+     </View>
+     <View>
+      <Text>Balance</Text>
+      <View>
+        <Text>{formatCurrency(HOME_BALANCE.amount)}</Text>
+      </View>
+     </View>
      </SafeAreaView>
   );
 }
--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
 import splashPattern from '@/assets/images/splash-pattern.png'
-import avatar from '@/assets/images/avatar.png'
+import unnamed from '@/assets/images/unnamed.png'
 
-export default { splashPattern, avatar }
+export default { splashPattern, unnamed }
